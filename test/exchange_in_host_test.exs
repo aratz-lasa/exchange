c221aadd420bdd147269f72c46b3ba744c70c586
Aratz M. Lasa <aratzml@opendeusto.es>
2019-05-14 01:00:35 +0200

    Added working test for 'Accept offer'

--- test/exchange_in_host_test.exs
+++ test/exchange_in_host_test.exs
@@ -74,6 +74,32 @@ defmodule ExchangeInHost do
     add_good(state)
   end
 
+  test "accept offer", state do
+    # Initialize offer (test in 'exchange_in_guest')
+    offer = send_offer(state)
+    # Initialize variables
+    guest_socket = state[:guest_socket]
+    exchange_id = state[:exchange_id]
+    # Accept offer
+    socket = state[:socket]
+    opcode_out = Prot.accept_offer()
+    data_out = Enum.join([exchange_id, offer.id], "#")
+    msg_out = <<opcode_out>> <> data_out
+    :ok = :gen_tcp.send(socket, msg_out)
+    # Check host socket
+    {:ok, msg_in} = :gen_tcp.recv(socket, 0)
+    [opcode_in | data_in] = msg_in
+    assert opcode_in == Prot.ok_opcode()
+    # Check guest socket
+    {:ok, msg_in} = :gen_tcp.recv(guest_socket, 0)
+    [opcode_in | data_in] = msg_in
+    assert opcode_in == Prot.offer_accepted()
+    [from, what] = String.split(to_string(data_in), "#", parts: 2)
+    assert exchange_id == from
+    offer_id = offer.id    
+    assert offer_id == what
+  end
+
   # Utils
   def sign_in(user, pass) do
     # Create TCP connection
@@ -145,5 +171,34 @@ defmodule ExchangeInHost do
     [from | good] = String.split(to_string(data), "#", parts: 2)
     assert exchange_id == from
     Good.decode(to_string(good))
+  end
+
+  def send_offer(state) do
+    guest_socket = state[:guest_socket]
+    exchange_id = state[:exchange_id]
+    guest_id = state[:guest_id]
+    # add good
+    good = add_good(state)
+    # inform about for goods
+    opcode_out = Prot.send_offer()
+    offer = %Offer{good: good.id}
+    data_out = exchange_id <> "#" <> Offer.encode(offer)
+    msg_out = <<opcode_out>> <> data_out
+    :ok = :gen_tcp.send(guest_socket, msg_out)
+    # Check in guest socket
+    {:ok, msg_in} = :gen_tcp.recv(guest_socket, 0)
+    [opcode_in | data_in] = msg_in
+    assert opcode_in == Prot.ok_opcode()
+    offer = Offer.decode(to_string(data_in))
+    # Check in host socket
+    socket = state[:socket]
+    {:ok, msg_in} = :gen_tcp.recv(socket, 0)
+    [opcode_in | data_in] = msg_in
+    assert opcode_in == Prot.rcv_offer()
+    [exchange, guest, data] = String.split(to_string(data_in), "#", parts: 3)
+    assert exchange_id == exchange
+    assert guest_id == guest
+    assert offer == Offer.decode(data)
+    offer
   end
 end
